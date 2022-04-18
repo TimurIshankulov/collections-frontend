6a856d9ead808f4abeeca742c27e7a456e5785c6
Timur Ishankulov <tim290592@gmail.com>
2022-04-18 17:19:10 +0300

    Add component StatusBar

---
+++ src/components/StatusBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row me-auto">
+    <p class="text-end">{{ amountOfDust }} Пыли</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StatusBar',
+  data() {
+    return {
+      dust: 0
+    }
+  },
+  computed: {
+    amountOfDust() {
+      return this.$store.state.dust
+    }
+  }
+}
+</script>
