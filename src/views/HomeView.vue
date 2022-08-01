b49718e89bdb29b713766847e0e7c5ceafb3bd93
Timur Ishankulov <tim290592@gmail.com>
2022-08-01 14:31:00 +0300

    Add HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,56 @@
 <template>
-  <h3>Home View</h3>
-  <span> </span>
+  <div class="row mt-4 me-auto">
+    <div class="col-lg-4"></div>
+    <div class="col-lg-4">
+      <h2 class="text-center">Коллекционируй карточки!</h2>
+    </div>
+    <div class="col-lg-4"></div>
+  </div>
+
+  <div class="row mt-5 me-auto">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-8 text-center">
+      <img src="@/assets/images/home_image.jpg" width="1700px">
+    </div>
+    <div class="col-lg-2"></div>
+  </div>
+
+  <div class="row mt-5 me-auto">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-4">
+      <h3>Получай одну бесплатную карточку каждый день!</h3>
+    </div>
+    <div class="col-lg-4">
+      <img src="@/assets/images/cards.jpg" width="600px">
+    </div>
+    <div class="col-lg-2"></div>
+  </div>
+
+  <div class="row mt-5 me-auto">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-4">
+      <img src="@/assets/images/collections.jpg" width="600px">
+    </div>
+    <div class="col-lg-4">
+      <h3>Собирай коллекции</h3>
+    </div>
+    <div class="col-lg-2"></div>
+  </div>
+
+  <div class="row mt-5 me-auto">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-4">
+      <h3>Соревнуйся с другими игроками</h3>
+    </div>
+    <div class="col-lg-4">
+      <img src="@/assets/images/rating.jpg" width="600px">
+    </div>
+    <div class="col-lg-2"></div>
+  </div>
+
+  <div class="row mt-5 me-auto">
+    <div class="empty-col"></div>
+  </div>
 </template>
 
 
@@ -9,14 +59,12 @@
 export default {
   name: 'HomeView',
   data() {
-    return {
-    }
+    return {}
   },
 
   mounted() {
   },
 
-  methods: {
-  }
+  methods: {}
 }
 </script>
